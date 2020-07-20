bb7bf11f63c931170e9d47579ef764ba5452a86b
ziyad <ziyadsfaxi@gmail.com>
2020-07-20 17:53:11 +0800

    Add example service call to home viewmodel

--- lib/src/views/home/home_viewmodel.dart
+++ lib/src/views/home/home_viewmodel.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_starter/generated/l10n.dart';
+import 'package:flutter_starter/logger/logger.dart';
+import 'package:flutter_starter/src/locator.dart';
+import 'package:flutter_starter/src/models/example_model.dart';
+import 'package:flutter_starter/src/services/example_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:built_collection/built_collection.dart';
 
 class HomeViewModel extends BaseViewModel {
   String _title = S.current.homePage;
   String get title => _title;
-  // ExampleApiService _exampleService = locator<ApiService>().getService<ExampleApiService>();
+  BuiltList<ExampleModel> _todos;
+  BuiltList<ExampleModel> get todos => _todos;
+  ExampleService _exampleService = locator<ExampleService>();
 
   int _counter = 0;
   int get counter => _counter;
@@ -14,7 +21,9 @@ class HomeViewModel extends BaseViewModel {
     notifyListeners();
   }
 
-  void useExampleService() async {
-    // _exampleService.login(body, id);
+  void getTodos() async {
+    _todos = await _exampleService.getTodos();
+    log(_todos.first);
+    notifyListeners();
   }
 }
