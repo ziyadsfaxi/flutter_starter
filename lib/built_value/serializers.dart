550aff4eda7be3ef7eaa0640c2596e7059204306
ziyad <ziyadsfaxi@gmail.com>
2020-07-20 17:49:56 +0800

    Add example model to serializers array

--- lib/built_value/serializers.dart
+++ lib/built_value/serializers.dart
@@ -1,9 +1,11 @@
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
+import 'package:flutter_starter/src/models/example_model.dart';
 
 part 'serializers.g.dart';
 
 @SerializersFor(const [
   // TODO: add models here
+  ExampleModel,
 ])
 final Serializers serializers = (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
